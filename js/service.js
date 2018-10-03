03a0760edca815e29e4fba9a8ed8ff6c1b1072e9
DT-Kim <398546027@qq.com>
2018-10-03 20:19:34 +0800

    base

---
+++ js/service.js
@@ -0,0 +1,15 @@
+/*
+ *  URL for sever
+ */
+
+//root php
+url = 'http://192.168.0.199:8081/zsPC/root/';//602=>DT-Kim
+//url = 'http://192.168.43.94:8081/zsPC/root/';//OwnPhone=>DT-Kim
+
+//root image
+UrlImage = 'http://192.168.0.199:8081/zsPC/';
+//UrlImage = 'http://192.168.43.94:8081/zsPC/';
+
+//java API
+//urlAPI = 'http://192.168.0.100:8080/';//localTest
+urlAPI = 'http://112.74.34.150:8080/';
