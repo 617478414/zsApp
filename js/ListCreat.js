1e0fd071f6599de4e0a34cf4afb36f6046b579e7
DT-Kim <398546027@qq.com>
2018-10-07 10:19:00 +0800

    表单详情

--- js/ListCreat.js
+++ js/ListCreat.js
@@ -15,16 +15,33 @@ function CreatNodeLeft(NodeName,i){
  * CheRes=》审批结果
  */
 function CreatNodeRight(CardType,UploadTime,CheMan,CheTim,CheEls,CheRes,NodeName,i){
+    //判断是流程
+    var CssType = '';
+//  switch(CardType){
+    switch(CheRes){
+        case '待签批':
+            CssType = 'cir-waitSign'
+        break;
+        case '驳回':
+            CssType = 'cir-reject'
+        break;
+        case '逾期':
+            CssType = 'cir-outdate'
+        break;
+        case '已签批':
+            CssType = 'cir-pass'
+        break;
+        default:break;
+    }
+    
     var htmlRight = ''
     htmlRight+="<div id='content"+ i + "' class='mui-control-content'><div class='mui-table-view'>";
     htmlRight+="<li class='mui-table-view-cell'>"+NodeName+"</li>";
-    for(var i=0;i<2;i++){
-        htmlRight+="<div class='mui-card'><div class='mui-card-header "+CardType+"'>提交时间："+UploadTime+"</div><div class='mui-card-content'><div class='mui-card-content-inner'>";
-        htmlRight+="<p><span>审批人：</span></p><p><span class='formMes'>测试工程2</span></p><hr/>"
-        htmlRight+="<p><span>审批时间：</span></p><p><span class='formMes'>测试工程2</span></p><hr/>";
-        htmlRight+="<p><span>备注：</span></p><p><span class='formMes'>测试工程2</span></p></div></div>"
-        htmlRight+="<div class='mui-card-footer'><span class='formMes'>审批结果:驳回</span></div></div>"
-    }
+    htmlRight+="<div class='mui-card'><div class='mui-card-header "+CssType+"'>提交时间："+UploadTime+"</div><div class='mui-card-content'><div class='mui-card-content-inner'>";
+    htmlRight+="<p><span>审批人：</span></p><p><span class='formMes'>"+CheMan+"</span></p><hr/>"
+    htmlRight+="<p><span>审批时间：</span></p><p><span class='formMes'>"+CheTim+"</span></p><hr/>";
+    htmlRight+="<p><span>备注：</span></p><p><span class='formMes'>"+CheEls+"</span></p></div></div>"
+    htmlRight+="<div class='mui-card-footer'><span class='formMes'>审批结果:"+CheRes+"</span></div></div>"
     htmlRight+='</div></div>';
     return htmlRight
 }
